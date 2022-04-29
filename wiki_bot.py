05705de1ccf12280becf556a59fc77cffac3a1c9
Murodjon Shermuhamedov <100022047+murodjon001@users.noreply.github.com>
2022-04-29 03:49:47 +0300

    Add files via upload
    Bu wikipediya bot

---
+++ wiki_bot.py
@@ -0,0 +1,39 @@
+import logging
+from aiogram.types import ContentTypes, Message
+
+from aiogram import Bot,Dispatcher,executor,types
+API_TOKEN = "bu yerda API token turadi"
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token= API_TOKEN)
+
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands= "start")
+async def send_welcome(message: Message):
+
+    await message.reply(f"Salom wikipedia botimizga xush kelibsiz!")
+
+
+@dp.message_handler()
+
+async def surov(message: Message):
+    print(message.text)
+    javob = input("javob kiriting: ")
+    await message.answer(javob)
+
+
+    try:
+        import wikipedia
+        wikipedia.set_lang("uz")
+        wiki = wikipedia.summary(message.text)
+        await message.answer(wiki)
+
+    except:
+        await message.answer("Kechirasiz bunday natijani topa olmadik !")
+#
+
+if __name__ == '__main__':
+    executor.start_polling(dp,skip_updates=True)
